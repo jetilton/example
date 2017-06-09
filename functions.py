eb74aafad4f2a4162bd36b0d4aca3b96ef958da6
jetilton <jfftilton@gmail.com>
2017-06-09 12:52:12 -0700

    added functions and script file

---
+++ functions.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+def add_two(a,b):
+    return a + b
+
+def myFunc():
+    return 3
+
---
+++ script.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+from functions import add_two
+from functions import myFunc
+df = pd.DataFrame({'a': [1,2,3], 'b':[4,5,6]})
+
+
+add_two(3,4)
+myFunc()
